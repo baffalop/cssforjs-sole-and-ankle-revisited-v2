10733b24a1b21926d55302ac0a87bdc9091ebfd4
Nikita Gaidakov <ngaidakov@podfather.com>
2025-08-31 16:54:35 +0100

    Add breakpoints via theme provider

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
@@ -8,12 +8,18 @@ const App = () => {
   const [sortId, setSortId] = React.useState('newest');
 
   return (
-    <>
+    <ThemeProvider theme={{
+      breakpoints: {
+        phone: '37.5rem',
+        tablet: '59.375rem',
+        desktop: '81.25rem',
+      }
+    }}>
       <Header />
       <Main>
         <ShoeIndex sortId={sortId} setSortId={setSortId} />
       </Main>
-    </>
+    </ThemeProvider>
   );
 };
 
