c2145b63b95efc521bb55aaf1661b2160325285b
Nikita Gaidakov <ngaidakov@podfather.com>
2025-08-31 17:10:37 +0100

    Set SuperHeader to decorative line at mobile

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -34,7 +34,11 @@ const Wrapper = styled.div`
   padding-right: 32px;
 
   @media (max-width: ${BREAKPOINTS.phone}) {
-    display: none;
+    height: 8px;
+
+    &>* {
+      display: none;
+    }
   }
 `;
 
